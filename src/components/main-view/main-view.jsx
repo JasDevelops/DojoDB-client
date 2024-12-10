85a99edd9b978e473417f1dea748155e21a2089b
Jas <50244053+JasDevelops@users.noreply.github.com>
2024-12-10 16:39:00 +0100

    Update main-view.jsx
    * Loading data from API

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,16 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
     const [movies, setMovies] = useState([]);
     const [selectedMovie, setSelectedMovie] = useState(null);
+
+    useEffect (() => {
+        fetch("https://dojo-db-e5c2cf5a1b56.herokuapp.com/movies")
+        .then ((response) => response.json())
+        .then ((data) => {
+            const moviesFromApi = data.map((movie) => ({
+                id:movie._id,
+                title: movie.title,
+                description: movie.description,
+                genre: {
+                    name: movie.genre.name,
+                    description:movie.genre.description
+                },
+                image: {
+                    imageUrl: movie.image.imageUrl ,
+                    imageAttribution: movie.image.imageAttribution
+                },
+                featured: true,
+                director: {
+                    name: movie.director.name,
+                    bio: movie.director.bio,
+                    birthYear: movie.director.birthYear,
+                    deathYear: movie.director.deathYear,
+                }
+            }));
+            setMovies(moviesFromApi);
+        });
+    }, []
+    );
     
     if (selectedMovie) {
     return (
-        <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+        <MovieView 
+        movie={selectedMovie} 
+        onBackClick={() => setSelectedMovie(null)} 
+        />
     );
     }
+
     if (movies.length === 0) {
     return <div>The list is empty!</div>;
     }
